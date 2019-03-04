ac84956be8fd3a253bc9aad1b4bcea46fce12e59
Noah <ncalle@udel.edu>
2019-03-03 21:50:03 -0500

    lab 3 commit

---
+++ Lab_3/src/MyCompare3.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Animal{
+	String name;
+	int numLegs;
+	
+	public int getLegs() {
+		return this.numLegs;
+	}
+}
+class Dog extends Animal{
+	
+	Dog(String name ,int numLegs) {
+		this.name=name;
+		this.numLegs=numLegs;
+	}
+    @Override
+    public String toString(){
+        return name +", "+ numLegs;
+    }
+}
+
+class MyCompare3 {
+	
+	
+	public static void main(String[] args){
+		
+		List<Dog> dogs = new ArrayList<Dog>();
+		dogs.add(new Dog("Fido", 4));
+		dogs.add(new Dog("Fido", 3));
+		dogs.add(new Dog("Alfie", 4));
+
+		System.out.println(dogs);
+		Collections.sort(dogs, new Comparator<Animal>(){
+			@Override
+			public int compare(Animal a, Animal b){
+			    return a.getLegs() - b.getLegs();
+			}
+		});
+		System.out.println(dogs);
+	}
+}
+
+
